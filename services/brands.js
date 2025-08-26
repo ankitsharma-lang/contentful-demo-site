3f1195bf9a1d514838e5b65aaffd9319d20d5124
ankitsharma-lang <ankit.sharma@contentful.com>
2025-08-26 14:08:27 +0200

    Update brands.js

--- services/brands.js
+++ services/brands.js
@@ -1,13 +1,17 @@
-var client = require('./contentfulClient').client
-var helper = require('../helpers/changeclient')
+// services/brands.js
+var client = require('./contentfulClient').client;
+var helper = require('../helpers/changeclient');
+var { getTypeId } = require('./contentTypeResolver');
 
-function getBrand (brandId, query) {
+const BRAND_KEY = process.env.BRAND_TYPE_KEY || 'brand';
+
+function getBrands(query) {
   let myClient = client;
-  let config = helper.switchClient(myClient, query)
-  config.query['sys.id'] = brandId
-  return config.myClient.getEntries(config.query)
+  let config = helper.switchClient(myClient, query);
+  return getTypeId(BRAND_KEY).then(typeId => {
+    config.query.content_type = typeId || BRAND_KEY;
+    return config.myClient.getEntries(config.query);
+  });
 }
 
-module.exports = {
-  getBrand
-}
+module.exports = { getBrands };
